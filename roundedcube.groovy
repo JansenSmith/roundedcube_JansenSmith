007ca6bf6fb2ba1a63117715a629625e88db59cf
JansenSmith <jansen@bstarengineering.com>
2023-04-15 15:34:15 -0400

    made roundier

--- roundedcube.groovy
+++ roundedcube.groovy
@@ -3,6 +3,6 @@ CSG roundedCube = new RoundedCube(	40,// X dimention
 				40,// Y dimention
 				40//  Z dimention
 				)
-				.cornerRadius(4)// sets the radius of the corner
+				.cornerRadius(10)// sets the radius of the corner
 				.toCSG()// converts it to a CSG tor display
 return roundedCube
