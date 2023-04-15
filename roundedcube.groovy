eed78137f4040a3c43c8843b1d16979e2e673eea
JansenSmith <jansen@bstarengineering.com>
2023-04-15 15:39:40 -0400

    testing failure

--- roundedcube.groovy
+++ roundedcube.groovy
@@ -1,8 +1,8 @@
 //create a rounded cube
 CSG roundedCube = new RoundedCube(	40,// X dimention
-				40,// Y dimention
+				8,// Y dimention
 				40//  Z dimention
 				)
-				.cornerRadius(1)// sets the radius of the corner
+				.cornerRadius(8)// sets the radius of the corner
 				.toCSG()// converts it to a CSG tor display
 return roundedCube
